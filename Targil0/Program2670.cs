e6e842fa144bb0dd6e72da73b51f11d693f52cbd
shirel avivi <shirellaviv@gmail.com>
2021-10-11 19:33:03 +0300

    initial code+welcome function

---
+++ Targil0/Program2670.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Targil0
+{
+   partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow World!");
+            welcome2670();
+            welcome6764();
+
+        }
+        static partial void welcome6764();
+
+        private static void welcome2670()
+        {
+            Console.Write("Enter your name: ");
+            string user = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", user);
+            Console.ReadKey();
+        }
+    }
+}
