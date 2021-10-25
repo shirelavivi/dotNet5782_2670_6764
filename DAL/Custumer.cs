08038a830b71375f83402ea660f98ee51581b4ae
HadasaPeretz <dperez@g.jct.ac.il>
2021-10-25 14:45:13 +0300

    add structs

---
+++ DAL/Custumer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DAL
+{
+    namespace DO
+    {
+        public struct Customer
+        {
+            public int id { get; set; }//Customer serial number
+            public string name { get; set; }//the customer's name
+            public string phone { get; set; }// customer's cell phone
+            public double longitude { get; set; }//Location
+            public double lattitude { get; set; }//Location
+        }
+    }
+}
