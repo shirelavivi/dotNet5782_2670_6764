7aa9edd3c322f2ecb4cdbc995969bc8d46769e31
shirel avivi <shirellaviv@gmail.com>
2021-10-27 23:12:21 +0300

    anything

--- DAL/Parcel.cs
+++ DAL/Parcel.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL
+
+namespace IDAL
 {
     namespace DO
     {
@@ -13,9 +14,7 @@ namespace DAL
             public int Id { get; set; }//Parcel serial number
             public int Senderld { get; set; }//
             public int Targetid { get; set; }// 
-            public enum Weightcategories { easy,middle, weighty }
             public Weightcategories Weight { get; set; }//Location
-            public enum Priorities { Normal, fast, emergency }
             public Priorities Priority { get; set; }//Location
             public DateTime Requested  { get; set; }
             public int Droneld { get; set; }
