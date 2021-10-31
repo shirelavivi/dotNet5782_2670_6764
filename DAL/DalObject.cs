3399de872db6354d295b19565711d3e6409c5fd5
HadasaPeretz <dperez@g.jct.ac.il>
2021-10-31 18:27:50 +0200

    bjhn

--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -8,7 +8,7 @@ using IDAL.DO;
 
 namespace IDAL
 {
-     namespace DalObject
+    namespace DalObject
     {
 
         class DataSource
@@ -21,37 +21,113 @@ namespace IDAL
             internal class config
             {
                 static int CounterPackets = 0;//  מספר מזהה סידורי לחבילות שיעודכן כל פעם שנוצרת חבילה חדשה 
-            }
-           public static void Initialize ()
-            {
-                Weightcategories t;
-                Station s=new Station();
-                Drone d = new Drone();
-                for (int i=0;i<3;i++)
-                {
-                   s.Id = rand.Next(); 
-                   s.Name = "Station"+i ;
-                   s.Longitude= rand.Next();
-                   s.Lattitude = rand.Next();
-                   s.ChargeSlots = i + 3;
-                    Stations.Add(s);
-                }
-                
 
-            }
-            Weightcategories s;
-            string sd = Console.ReadLine();
-            s= (weightcategories) int.Parse(sd);
-             switch(s)
+                public static void Initialize()
                 {
-                    case weightcategories.easy:
-                        break;
-                    case weightcategories.middle:
-                        break;
-                    case weightcategories.middle:
-                        break;
-                    default:
-                        break;
+
+                    int t;
+
+                    Station s = new Station();
+                    Drone d = new Drone();
+                    Customer c = new Customer();
+                    Parcel a = new Parcel();
+                    for (int i = 0; i < 3; i++)//מילוי 2 תחנות בסיס
+                    {
+                        s.Id = Rand.Next();
+                        s.Name = "Station" + i;
+                        s.Longitude = Rand.Next();
+                        s.Lattitude = Rand.Next();
+                        s.ChargeSlots = i + 3;
+                        Stations.Add(s);
+                    }
+
+                    for (int i = 0; i < 7; i++)//מילוי שישה רחפנים
+                    {
+                        d.id = Rand.Next();
+                        t = Rand.Next(3);
+                        switch (t)
+                        {
+                            case 0:
+                                d.MaxWeight = Weightcategories.easy;
+                                break;
+                            case 1:
+                                d.MaxWeight = Weightcategories.middle;
+                                break;
+                            case 2:
+                                d.MaxWeight = Weightcategories.weighty;
+                                break;
+
+                        }
+                        t = Rand.Next(3);
+                        switch (t)
+                        {
+                            case 0:
+                                d.status = DroneStatuses.available;
+                                break;
+                            case 1:
+                                d.status = DroneStatuses.maintenance;
+                                break;
+                            case 2:
+                                d.status = DroneStatuses.transport;
+                                break;
+
+                        }
+                        d.Battery = Rand.Next();
+                    }
+                    for (int i = 0; i < 11; i++)//מילוי 10 לקוחות
+                    {
+                        c.Id = Rand.Next();
+                        c.Name = "customer" + i;
+                        c.Phone = "05" + Rand.Next(4) + Rand.Next(9) + Rand.Next(9) + Rand.Next(10) + Rand.Next(10) + Rand.Next(10) + Rand.Next(10) + Rand.Next(10);
+                        c.Longitude = Rand.Next();
+                        c.Lattitude = Rand.Next();
+                    }
+
+                    for (int i = 0; i < 11; i++)//מילוי 10 חבילות
+                    {
+                        a.Id = CounterPackets;
+                        a.Senderld = Rand.Next();
+                        a.Targetid = Rand.Next();
+                        t = Rand.Next(3);
+                        switch (t)
+                        {
+                            case 0:
+                                a.Weight = Weightcategories.easy;
+                                break;
+                            case 1:
+                                a.Weight = Weightcategories.middle;
+                                break;
+                            case 2:
+                                a.Weight = Weightcategories.weighty;
+                                break;
+
+                        }
+
+                        t = Rand.Next(3);
+                        switch (t)
+                        {
+                            case 0:
+                                a.Priority = Priorities.Normal;
+                                break;
+                            case 1:
+                                a.Priority = Priorities.fast;
+                                break;
+                            case 2:
+                                a.Priority = Priorities.emergency;
+                                break;
+                            default:
+                                break;
+                        }
+                        a.Requested = DateTime.Now;
+                        a.Scheduled = DateTime.Now;
+                        a.PickedUp = DateTime.Now;
+                        a.Delivered = DateTime.Now;
+                        a.Droneld = 0;
+                        CounterPackets++;
+                    }
+
+                }
+
 
             }
 
@@ -79,7 +155,7 @@ namespace IDAL
                 public void ConnectParcelToDron(Parcel p)///שייוך חבילה לרחפן
                 {
                     List<Drone> run = drones;
-                    
+
 
                     for (int i = 0; i < run.Count(); i++)
                     {
@@ -105,7 +181,7 @@ namespace IDAL
 
                     for (int i = 0; i < run.Count(); i++)
                     {
-                        if(run[i].id==p.Droneld)
+                        if (run[i].id == p.Droneld)
                         {
                             temp.id = run[i].id;
                             temp.MaxWeight = run[i].MaxWeight;
@@ -120,7 +196,7 @@ namespace IDAL
                         }
 
                     }
-                   
+
                 }
                 public void PackageDalvery(Parcel p)
                 {
@@ -153,7 +229,8 @@ namespace IDAL
 
         }
 
-     }
+    }
 }
+
 
 
