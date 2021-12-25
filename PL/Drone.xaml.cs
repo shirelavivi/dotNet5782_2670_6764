4ad4c2737d1d88959ece34b5dea587fce7a6b369
shirel avivi <shirellaviv@gmail.com>
2021-12-25 20:32:26 +0200

    windowDrone

--- PL/Drone.xaml.cs
+++ PL/Drone.xaml.cs
@@ -19,9 +19,93 @@ namespace PL
     /// </summary>
     public partial class Drone : Window
     {
-        public Drone()
+        IBL.BO.BL bl;
+        
+        public Drone(IBL.BO.BL bldrone)//הוספה
         {
             InitializeComponent();
+            bl = bldrone;
+            GridAddDrone.Visibility = Visibility.Visible;
+            GridUpdateDrone.Visibility = Visibility.Hidden;
+
+        }
+        public Drone(IBL.BO.DroneToList drone,IBL.BO.BL bldrone)//עדכון 
+        {
+            bl = bldrone;
+            InitializeComponent();
+            TextIDUpdateDrone.Text = drone.Idnumber.ToString();
+            TextIDUpdateDrone.IsEnabled = false;
+            TextButteryUpdateDrone.Text = drone.ButerryStatus.ToString();
+            TextButteryUpdateDrone.IsEnabled = false;
+            ComboBoxMaxWeightUpdateDrone.SelectedItem = drone.Weightcategories;
+            ComboBoxMaxWeightUpdateDrone.IsEnabled = false;
+            ComboBoxStatusUpdateDrone.SelectedItem = drone.DroneStatuses;
+            ComboBoxStatusUpdateDrone.IsEnabled = false;
+            TextLatitudeUpdateDrone.Text = drone.ThisLocation.Lattitude.ToString();
+            TextLatitudeUpdateDrone.IsEnabled = false;
+            TextLongitudeUpdateDrone.Text = drone.ThisLocation.Longitude.ToString();
+            TextLongitudeUpdateDrone.IsEnabled = false;
+            TextModelUpdateDrone.Text = "";
+            TextModelUpdateDrone.IsEnabled = true;
+            GridAddDrone.Visibility = Visibility.Hidden;
+            GridUpdateDrone.Visibility = Visibility.Visible;
+
+
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            MessageBox.Show(e.NewValue.ToString());
+        }
+
+
+
+        private void ComboBoxMaxWeight_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+                MessageBox.Show((string)ComboBoxMaxWeight.SelectedItem);
+        }
+
+        private void TextModel_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBoxStatus_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show((string)ComboBoxStatus.SelectedItem);
+        }
+
+        
+
+        private void Button_ClikUpdateDrone(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_ClickAddDrone(object sender, RoutedEventArgs e)
+        {
+            IBL.BO.Drone drone = new IBL.BO.Drone();
+            drone.IdDrone = int.Parse(TextID.Text);
+            drone.Model = TextModel.Text;
+            drone.ButerryStatus = double.Parse(TextButtery.Text);
+            drone.ThisLocation.Longitude = double.Parse(TextLongitude.Text);
+            drone.ThisLocation.Lattitude = double.Parse(TextLatitude.Text);
+            drone.Weightcategories = (IBL.BO.Weightcategories)ComboBoxMaxWeight.SelectedItem;//weight
+            bl.
+           
+            //להוסיף אחר כך add
+            //TextID.IsEnabled = false;
         }
     }
 }
