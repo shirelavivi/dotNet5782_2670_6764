24a6c54204e1c8b097ef8dc0a78a0b95793d6258
shirel avivi <shirellaviv@gmail.com>
2022-01-13 15:55:15 +0200

    add new customer

--- PL/BaseStationWindow.xaml.cs
+++ PL/BaseStationWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using BO;
+using BlApi;
+
 
 namespace PL
 {
@@ -19,19 +22,75 @@ namespace PL
     /// </summary>
     public partial class BaseStationWindow : Window
     {
-        public BaseStationWindow()
+        IBL blStation;
+        BO.BaseStationToList stationWind;
+        public BaseStationWindow(IBL blstation)//add
         {
             InitializeComponent();
-        }
-
-        private void Button_ClikUpdateDrone(object sender, RoutedEventArgs e)
-        {
+            blStation = blstation;
+            UpdateStationButton.Visibility = Visibility.Hidden;
 
         }
-
-        private void IdnumberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        public BaseStationWindow(BO.BaseStationToList baseStation, IBL blstation)//update
         {
+            InitializeComponent();
+            blStation = blstation;
+            AddStationButton.Visibility = Visibility.Hidden;
+            stationWind = baseStation;
+            FullStation(baseStation);
+        }
+        private void FullStation(BO.BaseStationToList basestation)
+        {
+            IdnumberTextBox.Text = basestation.idnumber.ToString();
+            NameStationTextBox.Text = basestation.nameStation.ToString();
+            ChargingAvailableTextBox.Text = basestation.chargingAvailable.ToString();
+            ChargingNotAvailableTextBox.Text = basestation.chargingNotAvailable.ToString();
+            //צריך לעשות isEnable?
+        }
 
+        private void Button_ClikUpdateStation(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (NameStationTextBox.Text != "")
+                {
+                    blStation.UpdStation(Convert.ToInt32(NameStationTextBox.Text), ChargingAvailableTextBox.Text);
+                    MessageBox.Show("The station is update");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("please typ name of station");
+                }
+            }
+            catch (BO.MissingIdException)
+            {
+                MessageBox.Show("Erorr ID");
+            }
+        }
+
+
+        private void AddStationButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                BO.BaseStation baseStation = new BO.BaseStation();
+                baseStation.Idnumber = int.Parse(IdnumberTextBox.Text);
+                baseStation.NameStation = NameStationTextBox.Text;
+                baseStation.ChargingAvailable = int.Parse(ChargingAvailableTextBox.Text);
+                //baseStation.droneInCharging = new BO.DroneInParcel();
+                baseStation.locationOfStation = new BO.Location();
+                blStation.AddBaseStation(baseStation);
+                MessageBox.Show("The Station Was Successfully Added");
+                this.Close();
+                BaseStationWindow baseStationtWindow = new BaseStationWindow(blStation);
+                baseStationtWindow.Show();
+
+            }
+            catch (BO.DuplicateIdException ex)
+            {
+                MessageBox.Show(ex.Message, "Operation Failure", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
