57de650e28d34d84385289e33bef9586745bba7e
shirel avivi <shirellaviv@gmail.com>
2022-01-13 16:23:41 +0200

    shirel

--- PL/CustomerWindow.xaml.cs
+++ PL/CustomerWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace PL
         {
             InitializeComponent();
         }
+
+        private void Button_ClikUpdateDrone(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
